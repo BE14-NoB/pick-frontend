d370ab23e7ab3c51c8c9fca2088b69ade1c28dd3
minsun24 <jms49526@gmail.com>
2025-04-11 12:01:37 +0900

    Feature : 프로젝트 리스트

--- pick-front/src/project/views/ProjectDetailView.vue
+++ pick-front/src/project/views/ProjectDetailView.vue
@@ -2,3 +2,30 @@
     프로젝트 디테일
     
 </template>
+
+
+
+<script setup>
+    import {ref, computed, onMounted, watch} from 'vue';
+    import { useRoute } from 'vue-router'
+
+
+    const route = useRoute()
+    const id = route.params.id  
+    const projectData = ref(null)
+    
+
+    onMounted(async () => {
+        try {
+            const res = await fetch(`http://localhost:8080/${id}`)
+            const result = await res.json()
+            projectData.value = result
+        } catch (err) {
+            console.error('🚨 fetch 실패', err)
+        }
+    })
+
+    console.log(projectData);
+
+
+</script>
