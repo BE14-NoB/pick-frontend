8ed14b0c442e0a78164c0dd9e4626cd0d35ef39c
swjang7269 <azamiyuko7269@gmail.com>
2025-04-13 20:46:38 +0900

    Feature: 매칭 조회 페이지 구현

--- pick-front/src/views/Matching/MatchView.vue
+++ pick-front/src/views/Matching/MatchView.vue
@@ -1,32 +1,58 @@
 <template>
-    <div>매칭 페이지</div>
-    <MatchingSideBar/>
+<div class="matching-view">
+
+      <section class="matching-header">
+        <div class="page-header">
+          <div class="main-text">프로젝트 매칭</div>
+          <div class="sub-text">매칭을 조회하거나 생성된 프로젝트에 참여할 수 있습니다.</div>
+        </div>
+        <!-- MatchingSearch 컴포넌트를 절대 위치로 배치 -->
+        <div class="search-overlay">
+          <MatchingSearch />
+        </div>
+      </section>
+    </div>
 </template>
   
 
 <script setup>
   import { ref } from 'vue'
-  import MatchingSideBar from '@/components/matching/MatchingSideBar.vue'
-
-  const items = ref(Array.from({ length: 30 }, (k, v) => v + 1))
-
-  async function api () {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve(Array.from({ length: 10 }, (k, v) => v + items.value.at(-1) + 1))
-      }, 1000)
-    })
-  }
-  async function load ({ done }) {
-    // Perform API call
-    const res = await api()
-
-    items.value.push(...res)
-
-    done('ok')
-  }
+  import MatchingSearch from '@/components/matching/MatchingSearch.vue';
 </script>
 
 <style scoped>
+  .page-header {
+    height: 300px;
+    background-color: #cfe3f2;
+    margin: 0;
+    display: flex;
+    justify-content: center; /* 가로 가운데 */
+  }
 
+  .matching-header {
+    height: 693px;
+    position: relative;
+  }
+  .search-overlay {
+    position: absolute;
+    bottom: 260px;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 10;
+  }
+  .main-text {
+    position: absolute;
+    top: 90px;
+    left: 330px;
+    color: #020725;
+    font-size: 32px;
+    font-weight: bold;
+  }
+  .sub-text {
+    position: absolute;
+    color: #020725;
+    font-size: 16px;
+    top: 150px;
+    left: 330px;
+  }
 </style>
