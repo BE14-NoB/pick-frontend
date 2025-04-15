1d117355ff4ac93ec08fdf30790fec122fea6b4d
minsun24 <jms49526@gmail.com>
2025-04-15 10:41:13 +0900

    Refactor : 회의록 작성자, 참여자 디테일 페이지 칩 수정

--- pick-front/src/views/project/meeting/CreateMeeting.vue
+++ pick-front/src/views/project/meeting/CreateMeeting.vue
@@ -206,8 +206,6 @@ const imageMap = Object.fromEntries(
 )
 
 
-
-
 const filteredCommands = computed(() => {
   const list = commandMode.value === '/' ? commands : emojis
   return list.filter(cmd => cmd.name.includes(commandKeyword.value))
